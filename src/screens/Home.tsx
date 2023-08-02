a837fa334a5477cd7890b507693d2c8f516eb65d
Islam Sulaiman <ma.eslam@yahoo.com>
2023-08-02 11:05:33 +0300

    Dev: style the home alitle bit

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,44 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const HomeScreen: React.FC = () => {
+  const [userData, setUserData] = useState<{ fullName: string } | null>(null);
+
+  useEffect(() => {
+
+    const getUserData = async () => {
+      try {
+        const data = await AsyncStorage.getItem('user');
+        if (data) {
+          const parsedData = JSON.parse(data);
+          setUserData(parsedData);
+        }
+      } catch (error) {
+        console.error('Error retrieving user data:', error);
+      }
+    };
+
+    getUserData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome to the Home Screen!</Text>
-      <Text style={styles.message}>You have successfully logged in.</Text>
+      <View style={styles.header}>
+        {userData && <Text style={styles.userName}>Welcome, {userData.fullName}!</Text>}
+      </View>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+
+        <View style={styles.centerContent}>
+          <Text style={styles.centerText}>Never miss a task again</Text>
+          <Image source={require('../assets/pictures/002_logo_any-01_4x.png')} style={styles.logo} />
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={() => console.log('Button pressed')}>
+          <Text style={styles.buttonText}>Go to Another Screen</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 };
@@ -14,16 +46,48 @@ const HomeScreen: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#f0f0f0',
   },
-  title: {
-    fontSize: 24,
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 20,
+    backgroundColor: '#ffffff',
+    width: '100%',
+    position: 'absolute',
+    top: 0,
+    zIndex: 1,
+  },
+  userName: {
+    fontSize: 18,
     fontWeight: 'bold',
+  },
+  scrollViewContent: {
+    paddingTop: 100,
+    alignItems: 'center',
+  },
+  centerContent: {
+    alignItems: 'center',
+  },
+  centerText: {
+    fontSize: 24,
+    textAlign: 'center',
     marginBottom: 20,
   },
-  message: {
+  logo: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 20,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
     fontSize: 18,
     textAlign: 'center',
   },
